95eb9ae5d50f93ba8499c8e07ef0f418091abeeb
Maxime <maxime@processmaker.fr>
2019-04-06 19:59:19 +0100

    adding named routes

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../product_manager.dart';
-import './products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
@@ -15,15 +14,10 @@ class ProductsPage extends StatelessWidget {
               title: Text('Choose'),
             ),
             ListTile(
-              title: Text('Manage Products'),
-              onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            ProductsAdminPage()));
-              },
-            )
+                title: Text('Manage Products'),
+                onTap: () {
+                  Navigator.pushReplacementNamed(context, '/admin');
+                }),
           ],
         ),
       ),
