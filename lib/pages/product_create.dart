b8d62bfea90973052c1ed9bbb06a1deaa48f9e3f
Maxime <maximebarber@gmail.com>
2019-04-07 11:29:18 +0100

    84 - showing a modal

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -4,7 +4,15 @@ class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Create a Product'),
+      child: RaisedButton(
+        child: Icon(Icons.save),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context, builder: (BuildContext context) {
+                return Center(child: Text('This is a modal'),);
+              });
+        },
+      ),
     );
   }
 }
