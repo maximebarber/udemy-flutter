c8da69a750a7673fc52f49bfc7f68e3c82da9d58
Maxime <maximebarber@gmail.com>
2019-04-07 22:49:05 +0100

    92 - using our form to create a new product

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ProductCreatePage extends StatefulWidget {
+  final Function addProduct;
+
+  ProductCreatePage(this.addProduct);
+
   @override
   _ProductCreatePageState createState() => _ProductCreatePageState();
 }
@@ -44,7 +48,15 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
           ),
           RaisedButton(
             child: Text('Save'),
-            onPressed: () {},
+            onPressed: () {
+              final Map<String, dynamic> product = {
+                'title': titleValue,
+                'description': descriptionValue,
+                'price': priceValue,
+                'image': 'assets/food.jpg',
+              };
+              widget.addProduct(product);
+            },
           )
         ],
       ),
