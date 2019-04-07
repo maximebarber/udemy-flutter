d846b3b52b588cd4686fe27698c651aac88600b0
Maxime <maximebarber@gmail.com>
2019-04-07 21:49:24 +0100

    89 - configuring text fields

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -6,20 +6,30 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String titleValue = '';
+  String titleValue;
+  String descriptionValue;
+  double priceValue;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         TextField(
+          decoration: InputDecoration(labelText: "Title"),
           onChanged: (String value) {
             setState(() {
               titleValue = value;
             });
           },
         ),
-        Text(titleValue),
+        TextField(
+          decoration: InputDecoration(labelText: "Description"),
+          maxLines: 3,
+        ),
+        TextField(
+          decoration: InputDecoration(labelText: "Price"),
+          keyboardType: TextInputType.number,
+        ),
       ],
     );
   }
