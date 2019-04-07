79c97db21b1e33f6eeae387d69787771befabf9c
Maxime <maximebarber@gmail.com>
2019-04-07 21:21:00 +0100

    88 - saving user input

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  _ProductCreatePageState createState() => _ProductCreatePageState();
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Icon(Icons.save),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context, builder: (BuildContext context) {
-                return Center(child: Text('This is a modal'),);
-              });
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              titleValue = value;
+            });
+          },
+        ),
+        Text(titleValue),
+      ],
     );
   }
 }
