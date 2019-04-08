e6b2325366923cdddaa46ce9d53c84a76adb590b
Maxime <maximebarber@gmail.com>
2019-04-08 21:52:15 +0100

    95 - adding a switch

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -6,8 +6,9 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  String _emailValue = '';
+  String _emailValue;
   String _passwordValue;
+  bool _acceptTerms = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +39,15 @@ class _AuthPageState extends State<AuthPage> {
                 });
               },
             ),
+            SwitchListTile(
+              value: _acceptTerms,
+              onChanged: (bool value) {
+                setState(() {
+                  _acceptTerms = value;
+                });
+              },
+              title: Text('Accept terms'),
+            ),
             SizedBox(
               height: 10.0,
             ),
@@ -47,6 +57,8 @@ class _AuthPageState extends State<AuthPage> {
               textColor: Colors.white,
               onPressed: () {
                 Navigator.pushReplacementNamed(context, '/products');
+                print(_emailValue);
+                print(_passwordValue);
               },
             ),
           ],
