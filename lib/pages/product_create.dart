c8c0919552daf0d630ca61b474dd0bb7d0d84d4b
Maxime <maximebarber@gmail.com>
2019-04-08 10:01:32 +0100

    93 - improving the style of our form

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -46,8 +46,13 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               });
             },
           ),
+          SizedBox(
+            height: 10.0,
+          ),
           RaisedButton(
             child: Text('Save'),
+            color: Theme.of(context).accentColor,
+            textColor: Colors.white,
             onPressed: () {
               final Map<String, dynamic> product = {
                 'title': titleValue,
@@ -56,6 +61,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
                 'image': 'assets/food.jpg',
               };
               widget.addProduct(product);
+              Navigator.pushReplacementNamed(context, '/');
             },
           )
         ],
