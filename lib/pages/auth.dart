552aa39fd4ee5a6cd50cf83eb2c50151982548ff
Maxime <maximebarber@gmail.com>
2019-04-08 20:16:23 +0100

    exercice 4 - final solution

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue = '';
+  String _passwordValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,22 +17,34 @@ class AuthPage extends StatelessWidget {
       ),
       body: Container(
         margin: EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
+          //mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
               decoration: InputDecoration(labelText: 'Email'),
               keyboardType: TextInputType.emailAddress,
+              onChanged: (String value) {
+                setState(() {
+                  _emailValue = value;
+                });
+              },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Password'),
               obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  _passwordValue = value;
+                });
+              },
             ),
             SizedBox(
               height: 10.0,
             ),
             RaisedButton(
               child: Text('Login'),
+              color: Theme.of(context).primaryColor,
+              textColor: Colors.white,
               onPressed: () {
                 Navigator.pushReplacementNamed(context, '/products');
               },
